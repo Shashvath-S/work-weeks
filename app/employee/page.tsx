6585ba9b4a868961be2b1c9041d95062ddc403ed
Shashvath-S <119080138+Shashvath-S@users.noreply.github.com>
2025-01-20 18:34:46 -0800

    Created employee page and navbar basics

---
+++ app/employee/page.tsx
@@ -0,0 +1,10 @@
+export default function page () {
+    return (
+        <div>
+            <div style={{color: "black", margin: "10px", display: "flex", justifyContent: "space-between"}}>
+                <p style={{fontSize: "30px"}}>Timesheet</p>
+                <a href="/">Logout</a>
+            </div>
+        </div>
+    )
+}
