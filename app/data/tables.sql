52838901dbc96100e4663671ec4a5eacaa702b30
Magik <borolesaahil@gmail.com>
2025-03-29 16:32:37 -0700

    Added latest_clock_in, latest_clock_out, and total_hours columns to sqlite database

--- app/data/tables.sql
+++ app/data/tables.sql
@@ -1,14 +1,19 @@
-CREATE TABLE employees (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email TEXT UNIQUE,
-    role TEXT,
-    password TEXT,
-    name TEXT
+CREATE TABLE employees
+(
+    id               INTEGER PRIMARY KEY AUTOINCREMENT,
+    email            TEXT UNIQUE,
+    role             TEXT,
+    password         TEXT,
+    name             TEXT,
+    latest_clock_in  DATE,
+    latest_clock_out DATE,
+    total_hours      INTEGER
 );
 
-CREATE TABLE admin (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email TEXT UNIQUE,
+CREATE TABLE admin
+(
+    id       INTEGER PRIMARY KEY AUTOINCREMENT,
+    email    TEXT UNIQUE,
     password TEXT,
-    name TEXT
+    name     TEXT
 );
