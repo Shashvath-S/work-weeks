38c68a6dfd3acb379fb3c0d65a9edeabd9cacf3c
Shashvath-S <119080138+Shashvath-S@users.noreply.github.com>
2025-04-01 08:21:49 -0700

    Change Login/Register Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,4 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
-import { redirect } from "next/navigation";
-
 export default async function Home() {
-  const session = await getServerSession(authOptions);
-
-  console.log(session)
-
-  // if (session) {
-  //   if (session.user.role == "employee") {
-  //       redirect("/employee/home");
-  //   } else if (session.user.role == "admin") {
-  //       redirect("/admin/home");
-  //   }
-  // }
 
   return (
     <div
