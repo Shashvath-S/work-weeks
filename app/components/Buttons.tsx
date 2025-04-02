8b868dddca7eb61974e09eba0823bde5248246dc
Magik <borolesaahil@gmail.com>
2025-04-01 21:14:50 -0700

    Tracked untracked files

---
+++ app/components/Buttons.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { signOut } from "next-auth/react";
+import LogoutIcon from '@mui/icons-material/Logout';
+import HomeIcon from '@mui/icons-material/Home';
+
+export function SignOutButton () {
+    return <button style={{borderRadius: "15px"}} onClick={() => signOut({ callbackUrl: "/" })} className="btn btn-primary absolute top-2 right-2"><LogoutIcon /></button>
+}
+
+export function HomeButton () {
+    return <a style={{borderRadius: "15px"}} className="btn btn-primary absolute top-2 left-2" href="/admin/home"><HomeIcon /></a>
+}
