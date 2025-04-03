ecffa6b8ae48b5b793c74932b1a073a06e8c188a
Shashvath-S <119080138+Shashvath-S@users.noreply.github.com>
2025-04-02 19:20:47 -0700

    Edit Employee Page

--- app/employee/home/page.tsx
+++ app/employee/home/page.tsx
@@ -1,14 +1,20 @@
 import { authOptions } from "@/app/lib/AuthOptions";
 import EmployeeClock from "@/app/components/EmployeeClock";
-import {getServerSession} from "next-auth";
-import {redirect} from "next/navigation";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import AdminTopTitle from "@/app/components/AdminTopTitle";
 
 export default async function Page() {
-    const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
-    if (session?.user.role != "employee") {
-        redirect("/");
-    }
-    console.log(session.user.email);
-    return <EmployeeClock email={session.user.email}/>
+  if (session?.user.role != "employee") {
+    redirect("/");
+  }
+  console.log(session.user.email);
+  return (
+    <div style={{ padding: "20px", width: "100%", height: "95vh" }}>
+      <AdminTopTitle title="Work Weeks" link="" />
+      <EmployeeClock email={session.user.email} />
+    </div>
+  );
 }
