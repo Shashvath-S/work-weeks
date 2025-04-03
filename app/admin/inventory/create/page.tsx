703c089c167491ee14d233c318d7c36fed32573d
Shashvath-S <119080138+Shashvath-S@users.noreply.github.com>
2025-04-02 19:09:44 -0700

    Fix OTP issues

--- app/admin/inventory/create/page.tsx
+++ app/admin/inventory/create/page.tsx
@@ -30,104 +30,129 @@ export default function App() {
   return (
     <div style={{ padding: "20px", width: "100%", height: "95vh" }}>
       <AdminTopTitle title="Inventory" link="view" />
-      <form className="inventoryForm" onSubmit={onSubmit}>
-        <div className="mb-3">
-          <label htmlFor="itemName" className="form-label">
-            Item Name
-          </label>
-          <input
-            required
-            type="text"
-            className="form-control"
-            id="itemName"
-            placeholder="Enter item name"
-            aria-describedby="itemNameHelp"
-            value={itemName}
-            onChange={(e) => setItemName(e.target.value)}
-          />
-        </div>
+      <div className="container d-flex justify-content-center">
+        <div
+          className="card shadow-lg border-0 rounded-2xl"
+          style={{ width: "700px" }}
+        >
+          <div className="card-body p-4">
+            <form onSubmit={onSubmit}>
+              <div className="mb-3">
+                <label htmlFor="itemName" className="form-label">
+                  Item Name
+                </label>
+                <input
+                  required
+                  type="text"
+                  className="form-control"
+                  id="itemName"
+                  placeholder="Enter item name"
+                  aria-describedby="itemNameHelp"
+                  value={itemName}
+                  onChange={(e) => setItemName(e.target.value)}
+                />
+              </div>
 
-        <div className="mb-3">
-          <label htmlFor="category" className="form-label">
-            Category
-          </label>
-          <input
-            required
-            type="text"
-            className="form-control"
-            id="category"
-            placeholder="Enter category"
-            aria-describedby="categoryHelp"
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-          />
-        </div>
+              <div className="mb-3">
+                <label htmlFor="category" className="form-label">
+                  Category
+                </label>
+                <input
+                  required
+                  type="text"
+                  className="form-control"
+                  id="category"
+                  placeholder="Enter category"
+                  aria-describedby="categoryHelp"
+                  value={category}
+                  onChange={(e) => setCategory(e.target.value)}
+                />
+              </div>
 
-        <div className="mb-3">
-          <label htmlFor="quantityInStock" className="form-label">
-            Quantity In Stock
-          </label>
-          <input
-            required
-            type="number"
-            className="form-control"
-            id="quantityInStock"
-            placeholder="Enter quantity"
-            aria-describedby="quantityInStockHelp"
-            value={quantityInStock}
-            onChange={(e) => setQuantityInStock(e.target.value)}
-          />
-        </div>
+              <div className="mb-3">
+                <label htmlFor="quantityInStock" className="form-label">
+                  Quantity In Stock
+                </label>
+                <input
+                  required
+                  type="number"
+                  className="form-control"
+                  id="quantityInStock"
+                  placeholder="Enter quantity"
+                  aria-describedby="quantityInStockHelp"
+                  value={quantityInStock}
+                  onChange={(e) => setQuantityInStock(e.target.value)}
+                />
+              </div>
 
-        <div className="mb-3">
-          <label htmlFor="reorderLevel" className="form-label">
-            Reorder Level
-          </label>
-          <input
-            required
-            type="number"
-            className="form-control"
-            id="reorderLevel"
-            placeholder="Enter reorder level"
-            aria-describedby="reorderLevelHelp"
-            value={reorderLevel}
-            onChange={(e) => setReorderLevel(e.target.value)}
-          />
-        </div>
+              <div className="mb-3">
+                <label htmlFor="reorderLevel" className="form-label">
+                  Reorder Level
+                </label>
+                <input
+                  required
+                  type="number"
+                  className="form-control"
+                  id="reorderLevel"
+                  placeholder="Enter reorder level"
+                  aria-describedby="reorderLevelHelp"
+                  value={reorderLevel}
+                  onChange={(e) => setReorderLevel(e.target.value)}
+                />
+              </div>
 
-        <div className="mb-3">
-          <label htmlFor="unitPrice" className="form-label">
-            Unit Price
-          </label>
-          <input
-            required
-            type="number"
-            className="form-control"
-            id="unitPrice"
-            placeholder="Enter unit price"
-            aria-describedby="unitPriceHelp"
-            value={unitPrice}
-            onChange={(e) => setUnitPrice(e.target.value)}
-          />
-        </div>
+              <div className="mb-3">
+                <label htmlFor="unitPrice" className="form-label">
+                  Unit Price
+                </label>
+                <input
+                  required
+                  type="number"
+                  className="form-control"
+                  id="unitPrice"
+                  placeholder="Enter unit price"
+                  aria-describedby="unitPriceHelp"
+                  value={unitPrice}
+                  onChange={(e) => setUnitPrice(e.target.value)}
+                />
+              </div>
 
-        <div className="mb-3">
-          <label htmlFor="supplierID" className="form-label">
-            Supplier
-          </label>
-          <input
-            required
-            type="text"
-            className="form-control"
-            id="supplierID"
-            placeholder="Enter supplier"
-            aria-describedby="supplierIDHelp"
-            value={supplier}
-            onChange={(e) => setSupplier(e.target.value)}
-          />
+              <div className="mb-3">
+                <label htmlFor="supplierID" className="form-label">
+                  Supplier
+                </label>
+                <input
+                  required
+                  type="text"
+                  className="form-control"
+                  id="supplierID"
+                  placeholder="Enter supplier"
+                  aria-describedby="supplierIDHelp"
+                  value={supplier}
+                  onChange={(e) => setSupplier(e.target.value)}
+                />
+              </div>
+
+              <div className="d-grid gap-2">
+                <button
+                  type="submit"
+                  className="btn text-white btn-green-600 btn-lg"
+                  disabled={
+                    !itemName ||
+                    !category ||
+                    !quantityInStock ||
+                    !reorderLevel ||
+                    !unitPrice ||
+                    !supplier
+                  }
+                >
+                  Create Inventory Item
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-        <button className="btn btn-primary">Submit</button>
-      </form>
+      </div>
     </div>
   );
 }
--- app/components/Buttons.tsx
+++ app/components/Buttons.tsx
@@ -5,9 +5,9 @@ import LogoutIcon from '@mui/icons-material/Logout';
 import HomeIcon from '@mui/icons-material/Home';
 
 export function SignOutButton () {
-    return <button style={{borderRadius: "15px", color: "white"}} onClick={() => signOut({ callbackUrl: "/" })} className="btn btn-outline-primary absolute top-2 right-2 bg-green-600 border-blue-950"><LogoutIcon /></button>
+    return <button style={{borderRadius: "15px", color: "white", border: "none"}} onClick={() => signOut({ callbackUrl: "/" })} className="btn btn-outline-primary absolute top-2 right-2"><LogoutIcon /></button>
 }
 
 export function HomeButton () {
-    return <a style={{borderRadius: "15px", color: "white"}} className="btn btn-outline-primary absolute top-2 left-2 bg-green-600 border-blue-950" href="/admin/home"><HomeIcon /></a>
+    return <a style={{borderRadius: "15px", color: "white", border: "none"}} className="btn btn-outline-primary absolute top-2 left-2" href="/admin/home"><HomeIcon /></a>
 }
